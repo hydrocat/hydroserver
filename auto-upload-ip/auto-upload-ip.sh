f08d0cbc89c156d39a7f077b48af1a57143ff79a
hydroedge <hydrocat+hydroedge@pm.me>
2021-12-02 11:56:09 +0000

    Implement auto-upload-ip

---
+++ auto-upload-ip/auto-upload-ip.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -v
+
+#run this form the git repository
+
+HOSTNAME=`hostname`
+
+OLD_IP=`cat hosts/$HOSTNAME`
+IP=`curl -s icanhazip.com`
+
+echo $IP
+
+if test "$IP" == "$OLD_IP"
+then
+    echo IP address of $HOSTNAME did not change.
+else
+    git checkout ip
+    echo "$IP" > hosts/$HOSTNAME
+    git add "hosts/$HOSTNAME"
+    git commit -m "NEW IP ADDRESS AS OF `date`"
+    git push
+fi
